61133f6e3b6dc22b3ce3ae3c7096d319287e1b84
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2017-02-11 13:54:26 -0800

    Initial commit

---
+++ install-addons.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+ADDONS_LIST="$(dirname ${ABSOLUTE_PATH})/addons.org"
+
+PROFILE_DIR="${1}"
+test -n "${PROFILE_DIR}" || exit 1
+
+EXTENSIONS_DIR="${PROFILE_DIR}/extensions"
+test -d "${EXTENSIONS_DIR}" || exit 1
+
+cd "${EXTENSIONS_DIR}"
+for URL in $(grep 'https://' "${ADDONS_LIST}"); do
+    # wget --content-disposition "${URL}"
+    # curl -JOL "${URL}"
+    curl -LIs "${URL}"
+done
